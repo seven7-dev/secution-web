3ad20f444abffc75e2bb82e1042606f00c02da7b
SEVEN DEV <connect@seven-dev.com>
2023-11-02 14:39:41 +0100

    Component: Our services

---
+++ resources/views/components/our-services.blade.php
@@ -0,0 +1,124 @@
+<section class="our-service page-section-ptb">
+    <div class="objects-left">
+        <img class="objects-1 img-fluid wow fadeInLeft" data-wow-delay="0.2s" data-wow-duration="2.0s"
+            src="images/objects/01.png" alt=""
+            style="visibility: visible; animation-duration: 2s; animation-delay: 0.2s; animation-name: fadeInLeft;">
+        <img class="objects-2 img-fluid wow fadeInLeft" data-wow-delay="0.4s" data-wow-duration="2.0s"
+            src="images/objects/02.png" alt=""
+            style="visibility: visible; animation-duration: 2s; animation-delay: 0.4s; animation-name: fadeInLeft;">
+        <img class="objects-3 img-fluid wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="2.0s"
+            src="images/objects/03.png" alt=""
+            style="visibility: visible; animation-duration: 2s; animation-delay: 0.6s; animation-name: fadeInUp;">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="section-title text-center">
+                    <h6>Our Services</h6>
+                    <h2 class="title-effect">We provide the best service</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon mb-40">
+                            <div class="feature-icon">
+                                <span class="ti-desktop theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>Perfect design</h5>
+                                <p>Responsive Lorem Ipsum used since the 1500s is reproduced below for those.. </p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon mb-40">
+                            <div class="feature-icon">
+                                <span class="ti-headphone theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>24/7 Customer support</h5>
+                                <p>Great support quibusdam reproduced enim <span class="theme-color"
+                                        data-bs-toggle="tooltip" data-bs-placement="top" title=""
+                                        data-bs-original-title="Tooltip on top">expedita</span> sed quia nesciunt
+                                    incidunt..</p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon mb-40">
+                            <div class="feature-icon">
+                                <span class="ti-panel theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>Easy to Customize</h5>
+                                <p>Fully customizable template enim expedita sed quia nesciunt incidunt accusamus..</p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon mb-40">
+                            <div class="feature-icon">
+                                <span class="ti-shield theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>Powerful Performance</h5>
+                                <p>Fast as light Ipsum used since the 1500s is reproduced below for those Sections..
+                                </p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon xs-mb-40">
+                            <div class="feature-icon">
+                                <span class="ti-split-v theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>Parallax Sections</h5>
+                                <p>Fully customizable template enim expedita sed quia nesciunt incidunt accusamus..</p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="feature-text left-icon">
+                            <div class="feature-icon">
+                                <span class="ti-image theme-color" aria-hidden="true"></span>
+                            </div>
+                            <div class="feature-info">
+                                <h5>Revolution Slider</h5>
+                                <p>Fast as light Ipsum used since the 1500s is reproduced below for those Sections..
+                                </p>
+                                <a class="button icon-color" href="#">Read more <i
+                                        class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="objects-right">
+        <img class="objects-4 img-fluid wow fadeInRight" data-wow-delay="0.2s" data-wow-duration="2.0s"
+            src="images/objects/04.png" alt=""
+            style="visibility: visible; animation-duration: 2s; animation-delay: 0.2s; animation-name: fadeInRight;">
+        <img class="objects-5 img-fluid wow fadeInRight" data-wow-delay="0.4s" data-wow-duration="2.0s"
+            src="images/objects/05.png" alt=""
+            style="visibility: visible; animation-duration: 2s; animation-delay: 0.4s; animation-name: fadeInRight;">
+    </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <x-hero-slider />
     <x-action-box headline="Phishing-Betrug im Umlauf!"
         text="Wir warnen vor Phishing-Betrug, der unter unserem Namen im Umlauf ist.<br>Beachten Sie dringend: Öffnen Sie keine verdächtigen Nachrichten." />
+    <x-our-services />
     <x-latest-works />
     <x-contact-us />
 @endsection
