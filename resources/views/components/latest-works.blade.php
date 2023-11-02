1ab14ad8cf64e088ee326fcad31858e6aed29939
SEVEN DEV <connect@seven-dev.com>
2023-11-02 14:35:33 +0100

    Component: Latest Works

---
+++ resources/views/components/latest-works.blade.php
@@ -0,0 +1,81 @@
+<section class="portfolio-home gray-bg o-hidden">
+    <div class="container-fluid p-0">
+      <div class="row">
+         <div class="col-lg-4">
+           <div class="portfolio-title section-title mt-md-5">
+               <h6>Super creative</h6>
+               <h2 class="title-effect">Our Latest Works</h2>
+               <p class="mb-20">Work on the best projects for the best clients. Our clients are some of the most forward-looking companies in the world.</p>
+               <span>Webster has powerful options & tools, unlimited designs, responsive framework and amazing support. We are dedicated to providing you with the best experience possible. Purchase webster to find out why the sky's the limit when using Webster.</span>
+               <a class="button mt-30" href="portfolio-classic-3-columns-fullwidth.html"> See all projects </a>
+             </div>
+             <div>
+           </div>
+         </div>
+         <div class="col-lg-8">
+            <div class="isotope popup-gallery columns-3 no-padding">
+               <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/01.jpg" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html">Post item image</a> </h4>
+                         <span class="text-white"> <a href="#"> Photography  </a>| <a href="#">Ecommerce </a> </span>
+                    </div>
+                    <a class="popup portfolio-img" href="images/portfolio/small/01.jpg"><i class="fa fa-arrows-alt"></i></a>
+                  </div>
+                </div>
+                <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/02.jpg" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html"> Post item image</a> </h4>
+                         <span class="text-white"> <a href="#"> Creative  </a>| <a href="#">Graphics </a> </span>
+                    </div>
+                    <a class="popup portfolio-img" href="images/portfolio/small/02.jpg"><i class="fa fa-arrows-alt"></i></a>
+                  </div>
+                </div>
+                <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/03.jpg" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html"> Post item image</a> </h4>
+                         <span class="text-white"> <a href="#"> Design  </a>| <a href="#">Illustration </a> </span>
+                    </div>
+                    <a class="popup portfolio-img" href="images/portfolio/small/03.jpg"><i class="fa fa-arrows-alt"></i></a>
+                  </div>
+                </div>
+                <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/04.gif" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html"> Post GIF image</a> </h4>
+                         <span class="text-white"> <a href="#"> Animation </a>| <a href="#">GIF </a> </span>
+                    </div>
+                    <a class="popup portfolio-img" href="images/portfolio/small/04.gif"><i class="fa fa-arrows-alt"></i></a>
+                  </div>
+                </div>
+                <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/05.jpg" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html"> Post item Youtube</a> </h4>
+                         <span class="text-white"> <a href="#"> Video  </a>| <a href="#">Youtube </a> </span>
+                    </div>
+                    <a class="popup popup-youtube" href="https://www.youtube.com/watch?v=LgvseYYhqU0"><i class="fa fa-play"></i></a>
+                  </div>
+                </div>
+                <div class="grid-item">
+                 <div class="portfolio-item">
+                    <img src="images/portfolio/small/06.jpg" alt="">
+                     <div class="portfolio-overlay">
+                         <h4 class="text-white"> <a href="portfolio-single-01.html"> Post item image </a> </h4>
+                         <span class="text-white"> <a href="#"> Photography  </a>| <a href="#">Illustration </a> </span>
+                    </div>
+                    <a class="popup portfolio-img" href="images/portfolio/small/06.jpg"><i class="fa fa-arrows-alt"></i></a>
+                  </div>
+                </div>
+            </div>
+         </div>
+      </div>
+    </div>
+   </section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,6 @@
     <x-hero-slider />
     <x-action-box headline="Phishing-Betrug im Umlauf!"
         text="Wir warnen vor Phishing-Betrug, der unter unserem Namen im Umlauf ist.<br>Beachten Sie dringend: Öffnen Sie keine verdächtigen Nachrichten." />
+    <x-latest-works />
     <x-contact-us />
 @endsection
