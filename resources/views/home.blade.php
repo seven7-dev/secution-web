d6784c9dc290baf09317c23c4f1d1a55e0c1dbcd
SEVEN DEV <connect@seven-dev.com>
2023-11-02 16:26:40 +0100

    Component: Text Images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
         text="Wir warnen vor Phishing-Betrug, der unter unserem Namen im Umlauf ist.<br>Beachten Sie dringend: Öffnen Sie keine verdächtigen Nachrichten." />
     <x-our-services />
     <x-video-cta />
+    <x-text-images />
     <x-latest-works />
     <x-contact-us />
 @endsection
