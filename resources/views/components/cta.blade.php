11f5287aa3255ece298e64390ccf9bcbbdf90560
SEVEN DEV <connect@seven-dev.com>
2023-11-02 16:29:13 +0100

    Component: CTA

---
+++ resources/views/components/cta.blade.php
@@ -0,0 +1,18 @@
+<section class="white-bg page-section-ptb position-relative">
+    <div class="container">
+      <div class="col-lg-12 col-md-12">
+              <div class="section-title text-center">
+              <h2>Section White</h2>
+              <p>Truly ideal solutions for your business. Create a website that you are gonna be proud of</p>
+            </div>
+               <div class="action-box center">
+                <h6><strong> Webster: </strong> The most powerful template ever on the market</h6>
+                <p>Create tailor-cut websites with the exclusive multi-purpose responsive template along with powerful features.</p>
+                <a class="button" href="#">
+                  <span>Purchase Now</span>
+                  <i class="fa fa-download"></i>
+               </a>
+            </div>
+          </div>
+       </div>
+  </section>
