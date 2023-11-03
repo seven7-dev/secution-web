c2cfb002f6409c349193e7f5d50078225e4dc2c9
SEVEN DEV <connect@seven-dev.com>
2023-11-03 13:32:01 +0100

    Noscript

--- resources/views/includes/noscript.blade.php
+++ resources/views/includes/noscript.blade.php
@@ -1,7 +1,18 @@
 <noscript>
-    <style>
-        header, main {
+    <style style="display:block !important">
+        header, main, footer {
             display: none;
         }
     </style>
+    <section class="no-script page-section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <p>Quidem error quae illo excepturi nostrum blanditiis laboriosam magnam explicabo. lorem ipsum dolor
+                        sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
 </noscript>
