bbca913ce9b6d8ced77e28e7e9b458180a102da7
SEVEN DEV <connect@seven-dev.com>
2023-11-03 14:28:14 +0100

    Started with Redesign

--- resources/views/components/hero-slider.blade.php
+++ resources/views/components/hero-slider.blade.php
@@ -4,7 +4,63 @@
         style="margin:0px auto;background:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
         <!-- START REVOLUTION SLIDER 5.4.6.3 fullwidth mode -->
         <div id="rev_slider_92_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.4.6.3">
-            <ul> <!-- SLIDE  -->
+            <ul> 
+                <!-- SLIDE  -->
+                <li data-index="rs-217" data-transition="cube" data-slotamount="default" data-hideafterloop="0"
+                    data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="default"
+                    data-thumb="images/hero-example.jpg" data-rotate="0"
+                    data-fstransition="fade" data-fsmasterspeed="300" data-fsslotamount="7" data-saveperformance="off"
+                    data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5=""
+                    data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                    <!-- MAIN IMAGE -->
+                    <img src="images/hero-example.jpg" alt=""
+                        data-lazyload="images/hero-example.jpg" data-bgposition="center center"
+                        data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="3" class="rev-slidebg"
+                        data-no-retina>
+                    <!-- LAYERS -->
+
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption   rs-parallaxlevel-4" id="slide-217-layer-1"
+                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                        data-y="['middle','middle','middle','middle']" data-voffset="['-70','-70','-70','-100']"
+                        data-fontsize="['60','60','40','20']" data-lineheight="['70','70','50','30']"
+                        data-width="['720','640','480','300']" data-height="none" data-whitespace="normal"
+                        data-type="text" data-responsive_offset="off" data-responsive="off"
+                        data-frames='[{"delay":500,"speed":1000,"frame":"0","from":"y:20px;sX:0.9;sY:0.9;opacity:0;","to":"o:1;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"}]'
+                        data-textAlign="['center','center','center','center']" data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+                        style="z-index: 5; min-width: 720px; max-width: 720px; white-space: normal; font-size: 60px; line-height: 70px; font-weight: 700; color: rgba(255,255,255,1); letter-spacing: px;font-family:Open Sans;text-transform:uppercase;letter-spacing:10px;">
+                        Powerful <br />Multipurpose </div>
+
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption   rs-parallaxlevel-4" id="slide-218-layer-2"
+                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                        data-y="['middle','middle','middle','middle']" data-voffset="['44','57','39','0']"
+                        data-fontsize="['17','17','17','15']" data-lineheight="['26','26','26','24']"
+                        data-width="['720','480','480','300']" data-height="none" data-whitespace="normal"
+                        data-type="text" data-responsive_offset="off" data-responsive="off"
+                        data-frames='[{"delay":700,"speed":1000,"frame":"0","from":"y:10px;sX:0.9;sY:0.9;opacity:0;","to":"o:1;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"}]'
+                        data-textAlign="['center','center','center','center']" data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+                        style="z-index: 6; min-width: 720px; max-width: 720px; white-space: normal; font-size: 17px; line-height: 26px; font-weight: 400; color: rgba(255,255,255,1); letter-spacing: px;font-family:Open Sans;">
+                        Webster has powerful options & tools, unlimited designs, responsive framework and amazing
+                        support. We are dedicated to providing you with the best experience possible. </div>
+
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption rev-btn  rs-parallaxlevel-4" id="slide-218-layer-4"
+                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                        data-y="['middle','middle','middle','middle']" data-voffset="['133','159','141','102']"
+                        data-width="none" data-height="none" data-whitespace="nowrap" data-type="button"
+                        data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":"","speed":"300","ease":"Linear.easeNone"}]'
+                        data-responsive_offset="off" data-responsive="off"
+                        data-frames='[{"delay":900,"speed":1000,"frame":"0","from":"y:20px;sX:0.9;sY:0.9;opacity:0;","to":"o:1;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"0","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(0,0,0,1);bg:rgba(238,238,238,1);bs:solid;bw:0 0 0 0;"}]'
+                        data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[30,30,30,30]" data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[30,30,30,30]"
+                        style="z-index: 7; white-space: nowrap; font-size: 15px; line-height: 40px; font-weight: 700; color: rgba(0,0,0,1); letter-spacing: ;font-family:Open Sans;background-color:rgba(255,255,255,1);border-color:rgba(0,0,0,1);outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;">
+                        READ MORE </div>
+                </li>
+                <!-- SLIDE  -->
                 <li data-index="rs-218" data-transition="cube" data-slotamount="default" data-hideafterloop="0"
                     data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="default"
                     data-thumb="revolution/assets/demo/duotone80/100x50_5adbf-76.jpg" data-rotate="0"
