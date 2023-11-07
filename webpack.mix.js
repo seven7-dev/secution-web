71c00ab8fa05a59331f92aa9cd5864c2a3b1c73c
SEVEN DEV <connect@seven-dev.com>
2023-11-07 18:02:45 +0100

    Transfer JS Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,14 +3,31 @@ const mix = require('laravel-mix');
 // JS Compiling
 mix.js('resources/js/app.js', 'public/js/app.min.js')
     .scripts([
-        //
+        'resources/js/webster/jquery-3.6.0.min.js',
+        'resources/js/webster/plugins-jquery.js',
+        'resources/js/webster/plugin_path.min.js',
+        'resources/js/webster/revolution/js/jquery.themepunch.tools.min.js',
+        'resources/js/webster/revolution/js/jquery.themepunch.revolution.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.actions.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.carousel.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.kenburn.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.layeranimation.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.migration.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.navigation.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.parallax.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.slideanims.min.js',
+        'resources/js/webster/revolution/js/extensions/revolution.extension.video.min.js',
+        'resources/js/webster/revolution/js/revolution.addon.particles.min.js',
+        'resources/js/webster/revolution/js/revolution-custom.js',
+        'resources/js/webster/custom.js',
+        'resources/js/webster/revolution-settings.js',
     ], 'public/js/all.min.js')
 
 // CSS Compiling
 mix
     .sass('resources/scss/app.scss', 'public/css/app.min.css').options({
-    processCssUrls: false
-})
+        processCssUrls: false
+    })
     .sourceMaps(false, 'source-map', false);
 
 // Copying CSS
