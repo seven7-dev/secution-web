c0568261bb35725f2195b70202d42a849a92ffbe
SEVEN DEV <connect@seven-dev.com>
2023-11-07 14:39:49 +0100

    Redesign

--- resources/views/components/testimonials.blade.php
+++ resources/views/components/testimonials.blade.php
@@ -1,53 +1,31 @@
-<section class="gray-bg page-section-ptb">
-    <div class="container">
-      <div class="row">
-          <div class="col-md-12">
-          <div class="owl-carousel owl-loaded owl-drag" data-nav-dots="true" data-items="2" data-md-items="2" data-sm-items="1">
-               
-                 
-                 
-             <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-1764px, 0px, 0px); transition: all 1s ease 0s; width: 4116px;"><div class="owl-item cloned" style="width: 558px; margin-right: 30px;"><div class="item">
-                  <div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/02.jpg"> </div>
-                     <div class="testimonial-info"> One of the most complete template here. Thanks a lot for such great features, pages, shortcodes and home variations. Incredible Job. And the best of all, great introductions</div>
-                     <div class="author-info"> <strong>Xerracol - <span>ThemeForest user</span></strong> </div>
-                 </div>
-                 </div></div><div class="owl-item cloned" style="width: 558px; margin-right: 30px;"><div class="item">
-                  <div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/03.jpg"> </div>
-                      <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
-                     <div class="author-info"> <strong>Warble - <span>ThemeForest user</span></strong> </div>
-                   </div>
-                  </div></div><div class="owl-item" style="width: 558px; margin-right: 30px;"><div class="item"><div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/01.jpg"> </div>
-                     <div class="testimonial-info"> Excellent Customer support! These guys reply within minutes sometimes and really help you with when you need them. The template itself is very extended as well. Happy with my purchase!</div>
-                     <div class="author-info"> <strong>15Twelve - <span>ThemeForest user</span></strong> </div>
-                 </div>
-                 </div></div><div class="owl-item active" style="width: 558px; margin-right: 30px;"><div class="item">
-                  <div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/02.jpg"> </div>
-                     <div class="testimonial-info"> One of the most complete template here. Thanks a lot for such great features, pages, shortcodes and home variations. Incredible Job. And the best of all, great introductions</div>
-                     <div class="author-info"> <strong>Xerracol - <span>ThemeForest user</span></strong> </div>
-                 </div>
-                 </div></div><div class="owl-item active" style="width: 558px; margin-right: 30px;"><div class="item">
-                  <div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/03.jpg"> </div>
-                      <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
-                     <div class="author-info"> <strong>Warble - <span>ThemeForest user</span></strong> </div>
-                   </div>
-                  </div></div><div class="owl-item cloned" style="width: 558px; margin-right: 30px;"><div class="item"><div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/01.jpg"> </div>
-                     <div class="testimonial-info"> Excellent Customer support! These guys reply within minutes sometimes and really help you with when you need them. The template itself is very extended as well. Happy with my purchase!</div>
-                     <div class="author-info"> <strong>15Twelve - <span>ThemeForest user</span></strong> </div>
-                 </div>
-                 </div></div><div class="owl-item cloned" style="width: 558px; margin-right: 30px;"><div class="item">
-                  <div class="testimonial theme-bg bottom_pos">
-                     <div class="testimonial-avatar"> <img alt="" src="images/team/02.jpg"> </div>
-                     <div class="testimonial-info"> One of the most complete template here. Thanks a lot for such great features, pages, shortcodes and home variations. Incredible Job. And the best of all, great introductions</div>
-                     <div class="author-info"> <strong>Xerracol - <span>ThemeForest user</span></strong> </div>
-                 </div>
-                 </div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><i class="fa fa-angle-left fa-2x"></i></button><button type="button" role="presentation" class="owl-next"><i class="fa fa-angle-right fa-2x"></i></button></div><div class="owl-dots"><button role="button" class="owl-dot active"><span></span></button><button role="button" class="owl-dot"><span></span></button></div></div>
+<section class="page-section-ptb gray-bg testimonial-slider">
+   <div class="container">
+     <div class="row">
+       <div class="col-md-12">
+         <div class="owl-carousel" data-nav-dots="true" data-items="2" data-md-items="2" data-sm-items="1">
+           <div class="item">
+             <div class="testimonial">
+               <div class="testimonial-avatar"> <img alt="" src="images/team/01.jpg"> </div>
+               <div class="testimonial-info"> Excellent Customer support! These guys reply within minutes sometimes and really help you with when you need them. The template itself is very extended as well. Happy with my purchase!</div>
+               <div class="author-info"> <strong>15Twelve - <span>ThemeForest user</span></strong> </div>
+             </div>
+           </div>
+           <div class="item">
+             <div class="testimonial">
+               <div class="testimonial-avatar"> <img alt="" src="images/team/02.jpg"> </div>
+               <div class="testimonial-info"> One of the most complete template here. Thanks a lot for such great features, pages, shortcodes and home variations. Incredible Job. And the best of all, great introductions</div>
+               <div class="author-info"> <strong>Xerracol - <span>ThemeForest user</span></strong> </div>
+             </div>
+           </div>
+           <div class="item">
+             <div class="testimonial">
+               <div class="testimonial-avatar"> <img alt="" src="images/team/03.jpg"> </div>
+               <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+               <div class="author-info"> <strong>Warble - <span>ThemeForest user</span></strong> </div>
+             </div>
+           </div>
          </div>
-      </div>
-    </div>
+       </div>
+     </div>
+   </div>
  </section>
