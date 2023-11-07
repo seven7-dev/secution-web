bc0fe3e15566b973b136608673325743b5635801
SEVEN DEV <connect@seven-dev.com>
2023-11-07 18:46:55 +0100

    Create theme in resources, remove static assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,33 +2,33 @@ const mix = require('laravel-mix');
 
 // Theme JS Compiling
 mix.scripts([
-        'resources/js/theme/jquery-3.6.0.min.js',
-        'resources/js/theme/plugins-jquery.min.js',
-        'resources/js/theme/plugin_path.min.js',
-        'resources/js/theme/dependencies/revolution/jquery.themepunch.tools.min.js',
-        'resources/js/theme/dependencies/revolution/jquery.themepunch.revolution.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.actions.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.carousel.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.kenburn.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.layeranimation.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.migration.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.navigation.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.parallax.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.slideanims.min.js',
-        'resources/js/theme/dependencies/revolution/extensions/revolution.extension.video.min.js',
-        'resources/js/theme/dependencies/revolution/revolution.addon.particles.min.js',
-        'resources/js/theme/dependencies/revolution/revolution-custom.min.js',
-        'resources/js/theme/custom.min.js',
-        'resources/js/theme/revolution-settings.min.js',
+        'resources/theme/js/jquery-3.6.0.min.js',
+        'resources/theme/js/plugins-jquery.min.js',
+        'resources/theme/js/plugin_path.min.js',
+        'resources/theme/js/dependencies/revolution/jquery.themepunch.tools.min.js',
+        'resources/theme/js/dependencies/revolution/jquery.themepunch.revolution.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.actions.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.carousel.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.kenburn.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.layeranimation.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.migration.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.navigation.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.parallax.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.slideanims.min.js',
+        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.video.min.js',
+        'resources/theme/js/dependencies/revolution/revolution.addon.particles.min.js',
+        'resources/theme/js/dependencies/revolution/revolution-custom.min.js',
+        'resources/theme/js/custom.min.js',
+        'resources/theme/js/revolution-settings.min.js',
     ], 'resources/js/theme.min.js')
 
 // Dependencies JS Compiling
 mix.scripts([
-    'resources/js/theme/dependencies/isotope/isotope.pkgd.min.js',
-    'resources/js/theme/dependencies/mega-menu/mega_menu.min.js',
-    'resources/js/theme/dependencies/owl-carousel/owl-carousel.min.js',
-    'resources/js/theme/dependencies/magnific-popup/jquery.magnific-popup.min.js',
-    'resources/js/theme/dependencies/nicescroll/jquery.nicescroll.min.js'
+    'resources/theme/js/dependencies/isotope/isotope.pkgd.min.js',
+    'resources/theme/js/dependencies/mega-menu/mega_menu.min.js',
+    'resources/theme/js/dependencies/owl-carousel/owl-carousel.min.js',
+    'resources/theme/js/dependencies/magnific-popup/jquery.magnific-popup.min.js',
+    'resources/theme/js/dependencies/nicescroll/jquery.nicescroll.min.js'
 ], 'resources/js/dependencies.min.js')
 
 // App JS Compiling
