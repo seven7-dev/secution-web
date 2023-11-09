d640772d16009b461ac6e724622b86ac5a53592c
SEVEN DEV <connect@seven-dev.com>
2023-11-09 13:01:42 +0100

    Google Fonts Store Locally

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,11 +6,6 @@
 
 <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 
-<!-- font -->
-<link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,500,500i,600,700,800,900|Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
-<link href="http://fonts.googleapis.com/css?family=Yellowtail:400%7CMontserrat:700" rel="stylesheet" property="stylesheet" type="text/css" media="all">
-
-
 <!-- Plugins -->
 <link rel="stylesheet" type="text/css" href="../css/plugins-css.css" />
 
