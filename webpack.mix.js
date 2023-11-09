ce3f783d9a0fc97187b4f93402ba3d6f9fbf30a9
SEVEN DEV <connect@seven-dev.com>
2023-11-09 17:32:56 +0100

    JS Mix Configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,16 @@ const mix = require('laravel-mix');
 
 // Theme JS Compiling
 mix.scripts([
+        // jQuery
         'resources/theme/js/jquery-3.6.0.min.js',
+        // All plugins
         'resources/theme/js/plugins-jquery.min.js',
+        // Plugin path
         'resources/theme/js/plugin_path.min.js',
+        // Revolution JS files
         'resources/theme/js/dependencies/revolution/jquery.themepunch.tools.min.js',
         'resources/theme/js/dependencies/revolution/jquery.themepunch.revolution.min.js',
+        // Revolution Slider 5.0 extensions
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.actions.min.js',
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.carousel.min.js',
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.kenburn.min.js',
@@ -16,11 +21,15 @@ mix.scripts([
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.parallax.min.js',
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.slideanims.min.js',
         'resources/theme/js/dependencies/revolution/extensions/revolution.extension.video.min.js',
+        // Revolution Slider Add-On
         'resources/theme/js/dependencies/revolution/revolution.addon.particles.min.js',
+        // Revolution custom
         'resources/theme/js/dependencies/revolution/revolution-custom.min.js',
+        // Custom
         'resources/theme/js/custom.min.js',
+        // Revolution Slider Settings
         'resources/theme/js/revolution-settings.min.js',
-    ], 'resources/js/theme.min.js')
+    ], 'resources/theme/js/theme.min.js')
 
 // Dependencies JS Compiling
 mix.scripts([
@@ -29,13 +38,13 @@ mix.scripts([
     'resources/theme/js/dependencies/owl-carousel/owl-carousel.min.js',
     'resources/theme/js/dependencies/magnific-popup/jquery.magnific-popup.min.js',
     'resources/theme/js/dependencies/nicescroll/jquery.nicescroll.min.js'
-], 'resources/js/dependencies.min.js')
+], 'resources/theme/js/dependencies.min.js')
 
 // App JS Compiling
 mix.scripts([
-        'resources/js/theme.min.js',
-        'resources/js/dependencies.min.js',
-    ], 'public/js/app.min.js')
+    'resources/theme/js/theme.min.js',
+    'resources/theme/js/dependencies.min.js',
+], 'public/js/app.min.js');
 
 // CSS Compiling
 mix
