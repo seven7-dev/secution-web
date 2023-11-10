ffdb11fbe81f22efe388bbe3027933f13372f271
SEVEN DEV <connect@seven-dev.com>
2023-11-10 12:10:42 +0100

    Remove unused files & Optimize Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,34 +2,34 @@ const mix = require('laravel-mix');
 
 // Theme JS Compiling
 mix.scripts([
-        // jQuery
-        'resources/theme/js/jquery-3.6.0.min.js',
-        // All plugins
-        'resources/theme/js/plugins-jquery.min.js',
-        // Plugin path
-        'resources/theme/js/plugin_path.min.js',
-        // Revolution JS files
-        'resources/theme/js/dependencies/revolution/jquery.themepunch.tools.min.js',
-        'resources/theme/js/dependencies/revolution/jquery.themepunch.revolution.min.js',
-        // Revolution Slider 5.0 extensions
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.actions.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.carousel.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.kenburn.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.layeranimation.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.migration.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.navigation.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.parallax.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.slideanims.min.js',
-        'resources/theme/js/dependencies/revolution/extensions/revolution.extension.video.min.js',
-        // Revolution Slider Add-On
-        'resources/theme/js/dependencies/revolution/revolution.addon.particles.min.js',
-        // Revolution custom
-        'resources/theme/js/dependencies/revolution/revolution-custom.min.js',
-        // Custom
-        'resources/theme/js/custom.min.js',
-        // Revolution Slider Settings
-        'resources/theme/js/revolution-settings.min.js',
-    ], 'resources/theme/js/theme.min.js')
+    // jQuery
+    'resources/theme/js/jquery-3.6.0.min.js',
+    // All plugins
+    'resources/theme/js/plugins-jquery.min.js',
+    // Plugin path
+    'resources/theme/js/plugin_path.min.js',
+    // Revolution JS files
+    'resources/theme/js/dependencies/revolution/jquery.themepunch.tools.min.js',
+    'resources/theme/js/dependencies/revolution/jquery.themepunch.revolution.min.js',
+    // Revolution Slider 5.0 extensions
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.actions.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.carousel.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.kenburn.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.layeranimation.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.migration.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.navigation.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.parallax.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.slideanims.min.js',
+    'resources/theme/js/dependencies/revolution/extensions/revolution.extension.video.min.js',
+    // Revolution Slider Add-On
+    'resources/theme/js/dependencies/revolution/revolution.addon.particles.min.js',
+    // Revolution custom
+    'resources/theme/js/dependencies/revolution/revolution-custom.min.js',
+    // Custom
+    'resources/theme/js/custom.min.js',
+    // Revolution Slider Settings
+    'resources/theme/js/revolution-settings.min.js',
+], 'resources/theme/js/theme.min.js')
 
 // Dependencies JS Compiling
 mix.scripts([
@@ -53,22 +53,14 @@ mix
     })
     .sourceMaps(false, 'source-map', false);
 
-// Copying Images
-mix.copy('resources/images', 'public/images');
-
-// Copying Videos
-mix.copy('resources/video', 'public/video');
-
-// Copying Google Font Files
-mix.copy('resources/theme/fonts/google/s', 'public/fonts/google/s');
-
-// Copying Font Awesome Fonts
-mix.copy('resources/theme/fonts/fontawesome', 'public/fonts/fontawesome');
-
-// Copying Themify Fonts
-mix.copy('resources/theme/fonts/themify', 'public/fonts/themify');
-
-// Copying Revoultion Fonts
-mix.copy('resources/theme/revolution/fonts/revicons', 'public/fonts/revicons');
-
-
+// Copying Assets
+mix
+    // Images
+    .copy('resources/images', 'public/images')
+    // Videos
+    .copy('resources/video', 'public/video')
+    // Fonts
+    .copy('resources/theme/fonts/google/s', 'public/fonts/google/s')
+    .copy('resources/theme/fonts/fontawesome', 'public/fonts/fontawesome')
+    .copy('resources/theme/fonts/themify', 'public/fonts/themify')
+    .copy('resources/theme/revolution/fonts/revicons', 'public/fonts/revicons');
