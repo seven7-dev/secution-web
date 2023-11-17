44c9844c6190830ed13ace76d96b175a7f5de8df
SEVEN DEV <connect@seven-dev.com>
2023-11-17 14:23:47 +0100

    Localization: Sitemap

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 
 class SitemapXmlController extends Controller
 {
+    private $ignoreRoutes = [
+        'sanctum.csrf-cookie',
+        'ignition.healthCheck',
+        'ignition.executeSolution',
+        'ignition.updateConfig',
+        'sitemap_xml'
+    ];
+
     public function index()
     {
-        return response()->view('sitemap_xml')->header('Content-Type', 'text/xml');
+        $publicRoutes = $this->getPublicRoutes();
+
+        return response()
+            ->view('sitemap_xml', ['publicRoutes' => $publicRoutes])
+            ->header('Content-Type', 'text/xml');
+    }
+
+    private function getPublicRoutes()
+    {
+        $routes = [];
+
+        foreach (Route::getRoutes()->getRoutesByName() as $routeName => $route) {
+            if (in_array($routeName, $this->ignoreRoutes)) continue;
+            $routes[] = $routeName;
+        }
+
+        return $routes;
     }
 }
