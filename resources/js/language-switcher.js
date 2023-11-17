12b100fd1ac93dad5658c3646a968850526f66fe
SEVEN DEV <connect@seven-dev.com>
2023-11-17 15:10:30 +0100

    Locale Switcher Fix

--- resources/js/language-switcher.js
+++ resources/js/language-switcher.js
@@ -5,70 +5,70 @@
  * By Kelvin Luck ( http://www.kelvinluck.com/ )
  * Thanks for permission! 
  **/
-(function ($) {
-	$(document).ready(function () {
-		$('.styleswitch').click(function () {
+(function($){
+	$(document).ready(function() {
+		$('.styleswitch').click(function(){
 			switchStylestyle(this.getAttribute("data-switchcolor"));
 			return false;
 		});
 		var c = readCookie('style');
 		if (c) switchStylestyle(c);
 	});
-	function switchStylestyle(styleName) {
-		$('link[rel*=style][title]').each(function (i) {
+	function switchStylestyle(styleName){
+		$('link[rel*=style][title]').each(function(i){
 			this.disabled = true;
 			if (this.getAttribute('title') == styleName) this.disabled = false;
 		});
 		createCookie('style', styleName, 365);
 	}
 })(jQuery);
-function createCookie(name, value, days) {
-	if (days) {
+function createCookie(name,value,days){
+	if (days){
 		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		var expires = "; expires=" + date.toGMTString();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
 	}
 	else var expires = "";
-	document.cookie = name + "=" + value + expires + "; path=/";
+	document.cookie = name+"="+value+expires+"; path=/";
 }
-function readCookie(name) {
+function readCookie(name){
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
+	for(var i=0;i < ca.length;i++){
 		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 	}
 	return null;
 }
-function eraseCookie(name) {
-	createCookie(name, "", -1);
+function eraseCookie(name){
+	createCookie(name,"",-1);
 }
 
 // DEMO Swticher Base
-jQuery('.demo_changer .demo-icon').click(function () {
-	if (jQuery('.demo_changer').hasClass("active")) {
-		jQuery('.demo_changer').animate({ "left": "-70px" }, function () {
-			jQuery('.demo_changer').toggleClass("active");
-		});
-	} else {
-		jQuery('.demo_changer').animate({ "left": "0px" }, function () {
-			jQuery('.demo_changer').toggleClass("active");
-		});
-	}
+jQuery('.locale_changer .locale-icon').click(function(){
+	if(jQuery('.locale_changer').hasClass("active")){
+		jQuery('.locale_changer').animate({"left":"-70px"},function(){
+			jQuery('.locale_changer').toggleClass("active");
+		});						
+	}else{
+		jQuery('.locale_changer').animate({"left":"0px"},function(){
+			jQuery('.locale_changer').toggleClass("active");
+		});			
+	} 
 });
 
 // Selector (MODULE #2)
-jQuery('.demo_changer .PatternChanger a').click(function () {
+jQuery('.locale_changer .PatternChanger a').click(function(){
 	var bgBgCol = jQuery(this).attr('href');
-	jQuery('.demo_changer .PatternChanger a').removeClass('current');
-	jQuery('body').css({ backgroundColor: '#ffffff' });
+	jQuery('.locale_changer .PatternChanger a').removeClass('current');
+	jQuery('body').css({backgroundColor:'#ffffff'});
 	jQuery(this).addClass('current');
-	jQuery('body').css({ backgroundImage: 'url(' + bgBgCol + ')' });
-	if (jQuery(this).hasClass('bg_t')) {
-		jQuery('body').css({ backgroundRepeat: 'repeat', backgroundPosition: '50% 0', backgroundAttachment: 'scroll' });
+	jQuery('body').css({backgroundImage:'url(' + bgBgCol + ')'});
+	if (jQuery(this).hasClass('bg_t')){
+		jQuery('body').css({backgroundRepeat:'repeat', backgroundPosition:'50% 0', backgroundAttachment:'scroll'});
 	} else {
-		jQuery('body').css({ backgroundRepeat: 'repeat', backgroundPosition: '50% 0', backgroundAttachment: 'scroll' });
+		jQuery('body').css({backgroundRepeat:'repeat', backgroundPosition:'50% 0', backgroundAttachment:'scroll'});
 	}
 	return false;
 });
