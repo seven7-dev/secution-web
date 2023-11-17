701b9332ac175faf0e9e6df750514544eb49a0b9
SEVEN DEV <connect@seven-dev.com>
2023-11-17 16:48:47 +0100

    Remove testimonial form

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -4,7 +4,6 @@
     <x-hero-short />
     <x-block-quote />
     <x-testimonials-list />
-    <x-testimonial-form />
     <x-action-box headline="Lorem ipsum dolor sit amet"
     text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu." linkLabel="Mehr" linkUrl="{{ route('home') }}" linkExternal="true" backgroundColor="red"/>
 @endsection
